ae32e8e1558eea6ee1a78a319fc95757c5256ede
Jhonata <jholzinhooh567@gmail.com>
2025-03-27 11:53:25 -0300

    "components dinamycs properties "

---
+++ src/app/components/big-card/big-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-big-card',
+  standalone: false,
+  templateUrl: './big-card.component.html',
+  styleUrl: './big-card.component.css'
+})
+export class BigCardComponent {
+  @Input()
+  photoCover: string ="";
+  @Input()
+  cardTitle: string ='';
+  @Input()
+  cardSubtitle: string = '';
+  
+  constructor(){
+
+  }
+}
---
+++ src/app/components/small-card/small-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card',
+  standalone: false,
+  templateUrl: './small-card.component.html',
+  styleUrl: './small-card.component.css'
+})
+export class SmallCardComponent {
+  @Input()
+  photoCover: string ="";
+  @Input()
+  title: string ='';
+  
+  constructor(){
+
+  }
+}
